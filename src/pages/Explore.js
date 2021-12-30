d436e20baf9b450e6f75094f1cb6b8952bb7c219
Alfio Federico Di Pinto <fefferico81@gmail.com>
2021-12-31 00:30:10 +0900

    starting the pagination

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,40 @@
+import {Link} from 'react-router-dom'
+import rentCategoryImage from "../assets/categories/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/categories/sellCategoryImage.jpg";
+
+
+
 function Explore() {
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+      <main>
+        {/*  sliders */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Properties for rent</p>
+          </Link>
+          <Link to="/category/sale">
+            <img
+              src={sellCategoryImage}
+              alt="sale"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Properties for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
 export default Explore
